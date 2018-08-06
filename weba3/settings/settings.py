4469dd590e80b7d8ac455bfe36596b15593e8f5f
zimola <matt.zimola@gmail.com>
2018-08-05 21:43:28 -0300

    added whitenoise for staticfiles

--- weba3/settings/settings.py
+++ weba3/settings/settings.py
@@ -157,13 +157,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 STATIC_URL = '/static/'
-AUTH_USER_MODEL = 'accounts.User'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+AUTH_USER_MODEL = 'accounts.User'
+
 
 #import dj_database_url
 #DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True,)
